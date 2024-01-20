757fd56dd171a2494aa8e238025b40be68c57603
Vildan <khismatullin.vildan@mail.ru>
2024-01-20 12:52:02 +0500

    firebase del

--- server/db.js
+++ server/db.js
@@ -1,17 +1,13 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  connectionString: 'postgres://default:jLxwp4iZT1tQ@ep-fragrant-wildflower-41793733.eu-central-1.postgres.vercel-storage.com:5432/verceldb' + "?sslmode=require",
-})
-
-/*const pool = new Pool({
   user: 'postgres',
   password: '8453',
   host: 'localhost',
   port: 6000,
   database: 'audio-player'
-})*/
+})
 
-pool.connect().catch(e => console.log(e))
+pool.connect().then(() => {console.log("PostgreSQL Connected...")}).catch(e => console.log(e))
 
 module.exports = pool
